d8dd538e24b0ff2da8cd5f9c3db027207e9336a4
Dan Manning <dmanning23@hotmail.com>
2017-05-13 00:20:13 -0500

    updated all projets

--- InsertCoinBuddy/buildNugetPackages.ps1
+++ InsertCoinBuddy/buildNugetPackages.ps1
@@ -1,3 +1,3 @@
 rm *.nupkg
 nuget pack .\InsertCoinBuddy.nuspec -IncludeReferencedProjects -Prop Configuration=Release
-nuget push -source https://www.nuget.org -NonInteractive *.nupkg
+nuget push *.nupkg -Source https://www.nuget.org/api/v2/package
